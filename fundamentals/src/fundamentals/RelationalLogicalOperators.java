4de2ff745e45d2e27f43c7a6cd96a988936832fe
AdityaSingh25 <richiaditya25@gmail.com>
2023-11-04 23:09:30 +0530

    jnds

---
+++ fundamentals/src/fundamentals/RelationalLogicalOperators.java
@@ -0,0 +1,19 @@
+package fundamentals;
+
+public class RelationalLogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=11;
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		
+		System.out.println(a==b);//comparing 
+		System.out.println(a!=b);
+		
+	}
+
+}
